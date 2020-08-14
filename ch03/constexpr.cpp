1dc7b9bf62d79f602dd138d974d9a856acfde3d9
Zhiwei Liu <l.zhiwei@outlook.com>
2020-08-14 10:17:58 +0800

    constexpr

--- ch03/constexpr.cpp
+++ ch03/constexpr.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 
-long int fib(int n){
+constexpr long int fib(int n){
     return (n <= 1) ? n : fib(n-1) + fib(n-2);
 }
 
 int main(){
-    std::cout << fib(40) << std::endl;
+    long int a = fib(45);
+    std::cout << a << std::endl;
     return 0;
 }
